0cc547e1d0a7ff73a882621dedb68193c8ab2e45
Vectoryx <leomonta2002@gmail.com>
2021-05-21 17:34:31 +0200

    changed names to english and modified product table

--- HTTP_SERVER/res/Databases.sql
+++ HTTP_SERVER/res/Databases.sql
@@ -12,84 +12,79 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dump della struttura del database fornitori
-CREATE DATABASE IF NOT EXISTS `fornitori` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `fornitori`;
+-- Dump della struttura del database mb_db
+CREATE DATABASE IF NOT EXISTS `mb_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `mb_db`;
 
--- Dump della struttura di tabella fornitori.categorie
-CREATE TABLE IF NOT EXISTS `categorie` (
-  `nome` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`nome`)
+-- Dump della struttura di tabella mb_db.places
+CREATE TABLE IF NOT EXISTS `places` (
+  `ID_place` int(11) NOT NULL AUTO_INCREMENT,
+  `country` varchar(256) NOT NULL,
+  `region` varchar(256) NOT NULL,
+  `city` varchar(256) NOT NULL,
+  `street` varchar(256) NOT NULL,
+  `number` varchar(256) NOT NULL,
+  PRIMARY KEY (`ID_place`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella mb_db.products
+CREATE TABLE IF NOT EXISTS `products` (
+  `ID_product` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `options` blob DEFAULT NULL,
+  `tag` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID_product`),
+  KEY `tag` (`tag`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`tag`) REFERENCES `tags` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella mb_db.product_supplier
+CREATE TABLE IF NOT EXISTS `product_supplier` (
+  `ID_supplier` int(11) NOT NULL,
+  `ID_product` int(11) NOT NULL,
+  `price` double DEFAULT 0,
+  `quantity` int(11) DEFAULT 0,
+  PRIMARY KEY (`ID_supplier`,`ID_product`),
+  KEY `ID_product` (`ID_product`),
+  CONSTRAINT `product_supplier_ibfk_1` FOREIGN KEY (`ID_product`) REFERENCES `products` (`ID_product`),
+  CONSTRAINT `product_supplier_ibfk_2` FOREIGN KEY (`ID_supplier`) REFERENCES `suppliers` (`ID_supplier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dump dei dati della tabella fornitori.categorie: ~0 rows (circa)
-/*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
+-- L’esportazione dei dati non era selezionata.
 
--- Dump della struttura di tabella fornitori.fornitori
-CREATE TABLE IF NOT EXISTS `fornitori` (
-  `ID_fornitore` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(256) NOT NULL,
+-- Dump della struttura di tabella mb_db.suppliers
+CREATE TABLE IF NOT EXISTS `suppliers` (
+  `ID_supplier` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(256) NOT NULL,
   `email` varchar(256) DEFAULT NULL,
-  `telefono` varchar(256) DEFAULT NULL,
-  `paese_provenienza` int(11) NOT NULL,
-  PRIMARY KEY (`ID_fornitore`),
-  KEY `paese_provenienza` (`paese_provenienza`),
-  CONSTRAINT `fornitori_ibfk_1` FOREIGN KEY (`paese_provenienza`) REFERENCES `luoghi` (`ID_luogo`)
+  `website` varchar(256) DEFAULT NULL,
+  `ID_place` int(11) NOT NULL,
+  PRIMARY KEY (`ID_supplier`),
+  KEY `ID_place` (`ID_place`),
+  CONSTRAINT `suppliers_ibfk_1` FOREIGN KEY (`ID_place`) REFERENCES `places` (`ID_place`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella mb_db.tags
+CREATE TABLE IF NOT EXISTS `tags` (
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dump dei dati della tabella fornitori.fornitori: ~0 rows (circa)
-/*!40000 ALTER TABLE `fornitori` DISABLE KEYS */;
-/*!40000 ALTER TABLE `fornitori` ENABLE KEYS */;
+-- L’esportazione dei dati non era selezionata.
 
--- Dump della struttura di tabella fornitori.luoghi
-CREATE TABLE IF NOT EXISTS `luoghi` (
-  `ID_luogo` int(11) NOT NULL AUTO_INCREMENT,
-  `paese` varchar(256) NOT NULL,
-  `regione` varchar(256) NOT NULL,
-  `città` varchar(256) NOT NULL,
-  `via` varchar(256) NOT NULL,
-  `numero` varchar(256) NOT NULL,
-  PRIMARY KEY (`ID_luogo`)
+-- Dump della struttura di tabella mb_db.types
+CREATE TABLE IF NOT EXISTS `types` (
+  `type` text DEFAULT NULL,
+  `content` mediumtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dump dei dati della tabella fornitori.luoghi: ~0 rows (circa)
-/*!40000 ALTER TABLE `luoghi` DISABLE KEYS */;
-/*!40000 ALTER TABLE `luoghi` ENABLE KEYS */;
-
--- Dump della struttura di tabella fornitori.prodotti
-CREATE TABLE IF NOT EXISTS `prodotti` (
-  `ID_prodotto` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` text NOT NULL,
-  `opzioni` blob DEFAULT NULL,
-  `categoria` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID_prodotto`),
-  KEY `categoria` (`categoria`),
-  CONSTRAINT `prodotti_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categorie` (`nome`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Dump dei dati della tabella fornitori.prodotti: ~0 rows (circa)
-/*!40000 ALTER TABLE `prodotti` DISABLE KEYS */;
-/*!40000 ALTER TABLE `prodotti` ENABLE KEYS */;
-
--- Dump della struttura di tabella fornitori.prodotti_fornitori
-CREATE TABLE IF NOT EXISTS `prodotti_fornitori` (
-  `ID_fornitore` int(11) NOT NULL,
-  `ID_prodotto` int(11) NOT NULL,
-  `ID_luogo` int(11) NOT NULL,
-  `prezzo` double DEFAULT 0,
-  `quantita` int(11) DEFAULT 0,
-  PRIMARY KEY (`ID_fornitore`,`ID_prodotto`,`ID_luogo`),
-  KEY `ID_luogo` (`ID_luogo`),
-  KEY `ID_prodotto` (`ID_prodotto`),
-  CONSTRAINT `prodotti_fornitori_ibfk_1` FOREIGN KEY (`ID_luogo`) REFERENCES `luoghi` (`ID_luogo`),
-  CONSTRAINT `prodotti_fornitori_ibfk_2` FOREIGN KEY (`ID_prodotto`) REFERENCES `prodotti` (`ID_prodotto`),
-  CONSTRAINT `prodotti_fornitori_ibfk_3` FOREIGN KEY (`ID_fornitore`) REFERENCES `fornitori` (`ID_fornitore`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Dump dei dati della tabella fornitori.prodotti_fornitori: ~0 rows (circa)
-/*!40000 ALTER TABLE `prodotti_fornitori` DISABLE KEYS */;
-/*!40000 ALTER TABLE `prodotti_fornitori` ENABLE KEYS */;
+-- L’esportazione dei dati non era selezionata.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
